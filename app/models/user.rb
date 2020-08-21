b55e398782f02e9c1a00dd7cc986f32fad508935
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-22 00:47:17 +0900

    セキュアパスワード設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :email, uniqueness: true
   before_validation { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
