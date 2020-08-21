a65a58722518f3d37e6b323011c0b4c3544e39c2
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-21 23:35:54 +0900

    post確認画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,13 +62,10 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :user_image)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest, :user_image)
+  end
 end
